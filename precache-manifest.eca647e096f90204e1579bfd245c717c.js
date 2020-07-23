2131894185a777bb9dd5af812277df40119a7f77
Robert Moreno Carrillo <rodemore@espol.edu.ec>
2020-07-23 17:53:14 -0500

    Updates

---
+++ precache-manifest.eca647e096f90204e1579bfd245c717c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa2746caba211f2209c60d70a4f5250",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/index.html"
+  },
+  {
+    "revision": "95d789d9406a36ef90f1",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/css/2.676aa8f5.chunk.css"
+  },
+  {
+    "revision": "df05182d4d737ff51e67",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/css/main.ad04ac6e.chunk.css"
+  },
+  {
+    "revision": "95d789d9406a36ef90f1",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/js/2.66b318a7.chunk.js"
+  },
+  {
+    "revision": "84194fe92eb6207d61083095176837ed",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/js/2.66b318a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df05182d4d737ff51e67",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/js/main.9c0f7b33.chunk.js"
+  },
+  {
+    "revision": "c6316a39690b29bb1217",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/js/runtime-main.f5a7e5da.js"
+  },
+  {
+    "revision": "cdae47750f6676f2300585cdc02ab618",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/Brand-01.cdae4775.png"
+  },
+  {
+    "revision": "76e2475df022a6a8940857a366ddd4c0",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/abimaell95.76e2475d.jpg"
+  },
+  {
+    "revision": "08dd39d73ac9b17bd63fa2b065f55fa3",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/client-hw.08dd39d7.jpg"
+  },
+  {
+    "revision": "1ca8f7b7649710d385d19697cf4cbb3c",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/dombpala.1ca8f7b7.svg"
+  },
+  {
+    "revision": "cc649c4b8f599bd017ac2fc864b82f69",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/experiencias.cc649c4b.jpg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "9dd8f581384e7019706723852488e2dd",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/grupos.9dd8f581.jpg"
+  },
+  {
+    "revision": "f036a7f246403b6739023ce06a4e6159",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/hero-bg.f036a7f2.jpg"
+  },
+  {
+    "revision": "5d7aa2cd2ba3db85d618dbed5349b2f1",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/ideas.5d7aa2cd.jpg"
+  },
+  {
+    "revision": "37e10411c797654c7b60e139a799b89e",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/nicolsss.37e10411.jpeg"
+  },
+  {
+    "revision": "1beca002f81db757f1c24fe8f8225ac2",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/proyectos.1beca002.jpg"
+  },
+  {
+    "revision": "faeab66b4874aefddaf11bfabacb530f",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/rodemore.faeab66b.jpeg"
+  },
+  {
+    "revision": "c9c23226b4f39e28ef547d2f75afed48",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/student-hw.c9c23226.jpg"
+  },
+  {
+    "revision": "81f48c25d405cd4e4a2d682993383fa7",
+    "url": "/https://nordicos-dawm.github.io/SPOOL/static/media/team.81f48c25.jpg"
+  }
+]);
