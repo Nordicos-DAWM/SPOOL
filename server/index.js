386ee7217dedd0bd24639b99fdb41080fa933b6d
Camilo <gcemas20@gmail.com>
2020-08-19 02:27:43 -0500

    init backend

---
+++ server/index.js
@@ -0,0 +1,28 @@
+//Requiero express y lo guardo en una constante
+const express = require('express');
+//Requiero morgan y lo guardo en una constante
+const morgan = require('morgan');
+//Requiero cors y lo guardo en una constante
+//Llamo al objeto express y lo guardo en una constante
+const app = express();
+//Requiero mongoose de la db y lo guardo en una constante
+const { mongoose } = require('./database');
+
+
+/*Configuracion del servidor*/
+//Le asigno una variable y una constante que asume el puerto 3000 por defecto
+app.set('port' , process.env.PORT || 3000);
+
+/*Middlewares*/
+app.use(morgan('dev'));
+app.use(express.json());
+
+/*Rutas*/
+app.use('/api/***' ,require('./routes/***'));
+
+
+/*Iniciando con el servidor*/
+//Le digo que escucha al puerto 3000 y coloco un mensaje por consola
+app.listen(app.get('port') , () => {
+	console.log('Server on port' , app.get('port'));
+});
