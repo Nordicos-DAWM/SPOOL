19a739e5a9ec90b6e6ebe540d18a4419def7b966
Camilo <gcemas20@gmail.com>
2020-08-20 11:09:39 -0500

    mailer implementation

---
+++ server/src/models/index.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+DATABASE_URL = "mongodb://localhost:27017/spool"
+
+mongoose.connect(DATABASE_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB connnection successful!'))
+    .catch(err => {
+        console.error.bind(console, 'MongoDB connection error:')
+    });
